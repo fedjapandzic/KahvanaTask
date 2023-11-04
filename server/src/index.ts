b4f117bf56054734c6ec773b0f2b1e96bfb6e0cb
Fedja Pandzic <fedjapandzic1@gmail.com>
2023-11-04 14:31:15 +0100

    added CORS handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,11 @@ const port = 8080;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow any origin to access
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.use('/users', usersRouter);
 
 app.listen(port, () => {
